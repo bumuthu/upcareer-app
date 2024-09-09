7eab7b058f24ab0cf730bd91da3560a42b8bbe38
bumuthu <bumuthu.dilshan@gmail.com>
2024-09-10 03:10:15 +0530

    Fix build failures

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
 'use client'
 
-import { PublicGrid } from '@/components/home/PublicGrid'
 import React from 'react'
-import { WithTopNavBarLayout } from '@/components/layout/WithTopNavBarLayout'
 
-const Public: React.FC = () => {
+const Home: React.FC = () => {
   return (
     <main >
-      <WithTopNavBarLayout>
-        <div className="flex min-h-screen flex-col items-center justify-between p-12">
-          <PublicGrid/>
-        </div>
-      </WithTopNavBarLayout>
+      <div className="flex min-h-screen flex-col items-center justify-between p-12">
+        Welcome to UpCareer
+      </div>
     </main>
   )
 }
 
-export default Public;
+export default Home;

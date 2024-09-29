bd4bd41834246fa08dfa62a09107599f01ebfc93
bumuthu <bumuthu.dilshan@gmail.com>
2024-09-29 11:56:34 +0530

    Add button clicks in top nav bar

--- src/components/layouts/TopNavBar.tsx
+++ src/components/layouts/TopNavBar.tsx
@@ -3,26 +3,25 @@ import { Col, Row, Button } from 'antd';
 import { LoginLink } from '@kinde-oss/kinde-auth-nextjs/components';
 import { useAuthContext } from '../../context/AuthContext';
 import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
+import { useRouter } from 'next/navigation';
 
 const TopNavBar = () => {
     const authContext = useAuthContext();
     const authUser = useKindeBrowserClient();
+    const router = useRouter();
 
-    const onClickInterviews = () => {
-        console.log("Clicked interviews")
-    }
     return (
         <div style={{ boxShadow: '0 4px 20px rgba(0, 0, 0, 0.05)', paddingBottom: 5, paddingTop: 5 }}>
             <Row justify="space-around" >
                 <Col span={4}>
-                    <img src={"/logo-full-480.svg"} style={{ width: 150, marginTop: 5 }} />
+                    <img src={"/logo-full-480.svg"} style={{ width: 150, marginTop: 5, cursor: "pointer" }} onClick={() => router.push("/")} />
                 </Col>
                 <Col span={8}>
                     <Row justify="end" >
-                        <Button type="text" size='middle' onClick={onClickInterviews} style={{ fontWeight: 600 }}>
+                        <Button type="text" size='middle' onClick={() => router.push("/")} style={{ fontWeight: 600 }}>
                             Interviews
                         </Button>
-                        <Button type="text" size='middle' onClick={onClickInterviews} style={{ fontWeight: 600, marginLeft: 20 }}>
+                        <Button type="text" size='middle' onClick={() => window.open(`${process.env.NEXT_PUBLIC_APP_BASE_URL}/`)} style={{ fontWeight: 600, marginLeft: 20 }}>
                             Tutorial
                         </Button>
                         {

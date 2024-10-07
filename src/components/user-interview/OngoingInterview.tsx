2d76920bca13c3f4d2dde30c287c39ee841e44b9
bumuthu <bumuthu.dilshan@gmail.com>
2024-10-07 15:32:34 +0530

    Update  OngoingUserInterview

--- src/components/user-interview/OngoingInterview.tsx
+++ src/components/user-interview/OngoingInterview.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import { Switch } from 'antd'
-import Paragraph from 'antd/es/typography/Paragraph'
 import React, { useEffect, useState } from 'react'
 import { PrivateRestService } from '../../services/client-side/api-services/private-rest-service'
 import { AzureAIClientService } from '../../services/client-side/azure-ai-client-service'
 import { useInterviewContext } from '../../context/InterviewContext'
+import { ExitIcon } from '../../icons/ExitIcon'
+import { StopIcon } from '../../icons/StopIcon'
+import { MicIcon } from '../../icons/MicIcon'
+import Paragraph from 'antd/es/typography/Paragraph'
 
 
 interface OngoingUserInterviewProps {
@@ -56,6 +58,15 @@ const OngoingUserInterview = (props: OngoingUserInterviewProps) => {
         }}>
             <div style={{
                 width: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+            }}>
+                <Paragraph style={{ backgroundColor: 'black', color: 'white', padding: '5px 10px', borderRadius: '10px' }}>
+                    {interviewContext.ongoingDialog ? interviewContext.ongoingDialog?.text + interviewContext.ongoingText : 'Nothing'}
+                </Paragraph>
+            </div>
+            <div style={{
+                width: '100%',
                 height: '200px',
                 position: 'relative',
                 bottom: '0',
@@ -64,14 +75,14 @@ const OngoingUserInterview = (props: OngoingUserInterviewProps) => {
                 justifyContent: 'space-between',
                 alignItems: 'center',
             }}>
-                <button style={{
+                <div style={{
                     border: 'none',
+                    width: '90px',
                     padding: '10px 20px',
                     borderRadius: '5px',
                     cursor: 'pointer',
                 }}>
-                    Left
-                </button>
+                </div>
 
                 <div style={{
                     border: 'none',
@@ -79,46 +90,28 @@ const OngoingUserInterview = (props: OngoingUserInterviewProps) => {
                     borderRadius: '5px',
                     cursor: 'pointer',
                 }}>
-                    <Switch style={{ marginTop: "20px", marginBottom: "50px" }} checked={micOn} onChange={() => setMicOn(!micOn)} />
-                    <Paragraph>
-                        {interviewContext.ongoingDialog ? interviewContext.ongoingDialog?.text + interviewContext.ongoingText : 'Nothing'}
-                    </Paragraph>
+                    <div className="hover-box" onClick={() => setMicOn(!micOn)}>
+                        {
+                            micOn ?
+                                <StopIcon /> :
+                                <MicIcon />
+                        }
+                    </div>
                 </div>
 
-                <button style={{
+                <div style={{
                     border: 'none',
                     padding: '10px 20px',
+                    marginRight: '50px',
                     borderRadius: '5px',
                     cursor: 'pointer',
                 }}>
-                    Exit
-                </button>
+                    <div className="hover-box">
+                        <ExitIcon />
+                    </div>
+                </div>
             </div>
         </div>
-
-        // <div style={{
-        //     width: '100%',
-        //     height: 'calc(100vh - 20px)',
-        //     borderRadius: '5px',
-        //     display: 'flex',
-        //     flexDirection: 'column',
-        //     justifyContent: 'flex-end',
-        // }}>
-        //     <div style={{
-        //         width: '100%',
-        //         height: '100px',  // Adjust the height as needed
-        //         position: 'relative',
-        //         bottom: '0',
-        //         left: '0',
-        //     }}>
-        //         <div>
-        //         <Switch style={{ marginTop: "20px", marginBottom: "50px" }} checked={micOn} onChange={() => setMicOn(!micOn)} />
-        //         <Paragraph>
-        //             {interviewContext.ongoingDialog ? interviewContext.ongoingDialog?.text + interviewContext.ongoingText : 'Nothing'}
-        //         </Paragraph>
-        //         </div>
-        //     </div>
-        // </div>
     )
 
 }

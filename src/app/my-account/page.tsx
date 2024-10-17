2208213598e0c86b92187fe8018dca4217dd3a79
Ashen <ashendeelakahhk@gmail.com>
2024-10-17 23:00:48 +0530

    settings tabs main container

--- src/app/my-account/page.tsx
+++ src/app/my-account/page.tsx
@@ -1,13 +1,16 @@
 "use client"
 import LeftNavBar from '@/components/layouts/LeftNavBar'
 import WithTopNavBar from '@/components/layouts/WithTopNavBar'
+import SettingsTabs from '@/components/settings/SettingsTabs'
 import React from 'react'
 
 const MyAccountPage = () => {
     return (
         <WithTopNavBar>
             <LeftNavBar>
-                <div style={{marginTop:"100px"}}><h1>MyAccountPage</h1></div>
+                <div style={{ maxWidth: '1200px', margin: '100px auto' }}>
+                    <SettingsTabs/>
+                </div>
             </LeftNavBar>
         </WithTopNavBar>
     )

ba7c22319027c5f00c65a2c162a398365ec47013
Ashen <ashendeelakahhk@gmail.com>
2024-11-17 12:37:29 +0530

    skeloton and title card

--- src/components/interview-analysis/QuestionTreeContainer.tsx
+++ src/components/interview-analysis/QuestionTreeContainer.tsx
@@ -1,6 +1,6 @@
 import { useInterviewContext } from '@/context/InterviewContext'
 import { FormattedTreeData } from '@/services/client-side/interview-node-service'
-import { Card, Typography } from 'antd'
+import { Card, Skeleton, Typography } from 'antd'
 import React, { useEffect, useState } from 'react'
 
 const QuestionTreeContainer = () => {
@@ -10,7 +10,7 @@ const QuestionTreeContainer = () => {
 
 	useEffect(() => {
 		if (interviewContext.interviewNodeService) {
-			interviewContext.setSelectedCardId!(interviewContext.interviewNodeService?.formatTree()[0].id!)
+			interviewContext.setSelectedCardId!(interviewContext.interviewNodeService?.formatTree()[0]?.id!)
 		}
 	}, [interviewContext.interviewNodeService])
 	const handleCardClick = (cardId: string) => {
@@ -23,31 +23,51 @@ const QuestionTreeContainer = () => {
 	}
 	return (
 		<div>
-			<Card style={{ overflowY: "auto", maxHeight: "860px" }}>
-				{interviewContext.interviewNodeService?.formatTree() && interviewContext.interviewNodeService?.formatTree().map((tree: FormattedTreeData) => (
-					<div key={tree.id}>
-						{tree.depth == 1 &&
-							<Card onMouseEnter={() => setHoveredCardId(tree.id)}
-								onMouseLeave={() => setHoveredCardId(null)} onClick={() => handleCardClick(tree.id)} style={{
-									cursor: "pointer",
-									backgroundColor: selectedCardId === tree.id ? "#0D99FF" : hoveredCardId === tree.id ? "#f0f8ff" : "white",
-									transition: "background-color 0.3s ease",
-									border: "solid 1px", borderColor: "#0D99FF", width: "100%", marginBottom: "20px", marginLeft: checkIsParent(tree.depth)
-								}}>
-								<Typography style={{ fontWeight: "bold", color: selectedCardId === tree.id ? "white" : "#0D99FF" }}>{tree.node.question}</Typography>
-							</Card>}
-						{tree.children!.length > 0 && tree.children?.map((childNode) =>
-							< Card key={childNode.id} onClick={() => handleCardClick(childNode.id)} style={{
-								cursor: "pointer",
-								backgroundColor: selectedCardId === childNode.id ? "#d0e8ff" : "white",
-								transition: "background-color 0.3s ease",
-								border: "solid 1px", width: "100%", marginBottom: "20px", marginLeft: "20px"
-							}}>
-								<Typography>{childNode.node.question}</Typography>
-							</Card>)}
-					</div>
-				))}
-			</Card>
+			{interviewContext.interviewNodeService ?
+				<Card style={{  overflowY: "auto", maxHeight: "720px", padding: "30px" }}>
+					{interviewContext.interviewNodeService?.formatTree().length! > 0 ?
+						<>{interviewContext.interviewNodeService?.formatTree() && interviewContext.interviewNodeService?.formatTree().map((tree: FormattedTreeData) => (
+							<div key={tree.id}>
+								{tree.depth == 1 &&
+									<Card onMouseEnter={() => setHoveredCardId(tree.id)}
+										onMouseLeave={() => setHoveredCardId(null)} onClick={() => handleCardClick(tree.id)} style={{
+											cursor: "pointer",
+											backgroundColor: selectedCardId === tree.id ? "#0D99FF" : hoveredCardId === tree.id ? "#f0f8ff" : "white",
+											transition: "background-color 0.3s ease",
+											border: "solid 1px", borderColor: "#0D99FF", width: "100%", marginBottom: "20px", marginLeft: checkIsParent(tree.depth)
+										}}>
+										<Typography style={{ fontWeight: "bold", color: selectedCardId === tree.id ? "white" : "#0D99FF" }}>{tree.node.question}</Typography>
+									</Card>}
+								{tree.children!.length > 0 && tree.children?.map((childNode) =>
+									< Card key={childNode.id} onClick={() => handleCardClick(childNode.id)} style={{
+										cursor: "pointer",
+										backgroundColor: selectedCardId === childNode.id ? "#0D99FF": hoveredCardId === childNode.id ? "#f0f8ff" : "white",
+										transition: "background-color 0.3s ease",
+										border: "solid 1px", width: "100%", marginBottom: "20px", marginLeft: "20px",borderColor: "#0D99FF"
+									}}>
+										<Typography style={{ fontWeight: "bold", color: selectedCardId === childNode.id ? "white" : "#0D99FF" }}>{childNode.node.question}</Typography>
+									</Card>)}
+							</div>
+						))}</> : <center><Typography.Paragraph >No interview data available. Start your interview now to begin!</Typography.Paragraph></center>}
+				</Card> : 
+				//skeletons
+				<Card style={{ overflowY: "auto", height: "860px", padding: "50px" }}>
+					<Card style={{ marginBottom: "20px", marginRight: "40px" }}>
+
+						<Skeleton active paragraph={{ rows: 1, width: "100%" }} />
+					</Card>
+					<Card style={{ marginBottom: "20px", marginLeft: "60px" }}>
+
+						<Skeleton active paragraph={{ rows: 1, width: "100%" }} />
+					</Card>
+					<Card style={{ marginBottom: "20px", marginRight: "40px" }}>
+
+						<Skeleton active paragraph={{ rows: 1, width: "100%" }} />
+					</Card>
+					<Card style={{ marginBottom: "20px", marginLeft: "60px" }}>
+						<Skeleton active paragraph={{ rows: 1, width: "100%" }} />
+					</Card>
+				</Card>}
 		</div >
 	)
 }
